bbe57a13a5de5b97412b285fb8e5ece497169d87
Chester Wong <psyforce99@gmail.com>
2023-09-05 21:59:08 +0800

    [sp]: add constructor

--- Team12/Code12/src/spa/src/sp/parser/ProgramParser.cpp
+++ Team12/Code12/src/spa/src/sp/parser/ProgramParser.cpp
@@ -1,9 +1,5 @@
-//
-// Created by psyfo on 5/9/2023.
-//
-
 #include "ProgramParser.h"
 
 std::unique_ptr<TNode> ProgramParser::parse() {
-    return std::make_unique<TNode>();
+    return std::make_unique<TNode>(TNodeType::TNODE_PROGRAM);
 }
