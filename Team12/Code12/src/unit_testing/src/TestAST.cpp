6ecf6305bdfa167a4ba46a7ca3c2d10157722125
loose-bus-change <e0661408@u.nus.edu>
2023-09-05 22:22:26 +0800

    Create Sample storage class
    Wrote a sample storage class and unit tests to test basic functionality of storage  class.

--- Team12/Code12/src/unit_testing/src/TestAST.cpp
+++ Team12/Code12/src/unit_testing/src/TestAST.cpp
@@ -2,18 +2,46 @@
 #include "TNode.h"
 
 #include "catch.hpp"
+#include "../../spa/src/Storage.h"
+#include <vector>
+#include <string>
+#include <stdexcept>
+
 using namespace std;
 void require(bool b) {
     REQUIRE(b);
 }
 
-TEST_CASE("1st Test") {
+TEST_CASE("Table functionality tests") {
+    Storage myTable;
 
-    TNode T;
-	
-	
-	
-    require(1 == 1);
+    SECTION("Insert rows and retrieve second column") {
+        myTable.insertRow(1, "Row 1");
+        myTable.insertRow(2, "Row 2");
+        myTable.insertRow(3, "Row 3");
+
+        REQUIRE(myTable.getSecondColumnByFirstColumn(1) == "Row 1");
+        REQUIRE(myTable.getSecondColumnByFirstColumn(2) == "Row 2");
+        REQUIRE(myTable.getSecondColumnByFirstColumn(3) == "Row 3");
+    }
+
+    SECTION("Calculate the number of rows") {
+        REQUIRE(myTable.getRowCount() == 0);
+
+        myTable.insertRow(1, "Row 1");
+        myTable.insertRow(2, "Row 2");
+        myTable.insertRow(3, "Row 3");
+
+        REQUIRE(myTable.getRowCount() == 3);
+    }
+
+    SECTION("Retrieve second column with non-existent first column value") {
+        myTable.insertRow(1, "Row 1");
+
+        // Check for an exception when the row is not found
+        REQUIRE_THROWS_WITH(
+            myTable.getSecondColumnByFirstColumn(2),
+            "Row not found"
+        );
+    }
 }
-
-
