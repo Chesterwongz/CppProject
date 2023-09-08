171ad98489d1044ecf9bb07951fa5df7f881f199
houtenteo <houtenteo@gmail.com>
2023-09-08 23:58:21 +0800

    Create Keywords.h

---
+++ Team12/Code12/src/spa/src/qps/tokeniser/Keywords.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+const std::vector<std::string> designEntities = {
+	"stmt",
+	"read",
+	"print",
+	"call",
+	"while",
+	"if",
+	"assign",
+	"variable",
+	"constant",
+	"procedure"
+};
+
+const std::string select = "Select";
+
+const std::string pattern = "pattern";
+
+const std::vector<std::string> suchThat = {
+	"such",
+	"that"
+};
