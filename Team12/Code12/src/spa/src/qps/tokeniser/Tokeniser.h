636d7257e004f197db6e1f7fa6c29c808a32ff76
houtenteo <houtenteo@gmail.com>
2023-09-09 00:10:06 +0800

    Update Tokeniser.h

--- Team12/Code12/src/spa/src/qps/tokeniser/Tokeniser.h
+++ Team12/Code12/src/spa/src/qps/tokeniser/Tokeniser.h
@@ -1,15 +1,26 @@
 #pragma once
 
+#include "qps/token/QueryToken.h"
 #include <string>
 #include <vector>
-#include "qps/token/QueryToken.h"
 
 class Tokeniser {
-private:
-
-
 public:
-    QueryToken *convertToTokens(std::string &inputString);
+
+	std::vector<QueryToken>* convertToTokens(std::string* query);
+
+private:
+	std::vector<QueryToken> processFragment(std::string queryFragment, std::vector<QueryToken>* tokens);
+
+	std::vector<std::string> getDesignEntitySynonyms(std::vector<std::string> whitespaceDelimitedFragments, std::size_t* iPtr);
+
+	std::string getRelationship(std::vector<std::string> whitespaceDelimitedFragments, std::size_t* iPtr);
+
+	std::string getPatternSynonym(std::vector<std::string> whitespaceDelimitedFragments, std::size_t* iPtr);
+
+	std::vector<std::string> getArguments(std::vector<std::string> whitespaceDelimitedFragments, std::size_t* iPtr);
+
 };
+
 
 typedef std::vector<std::string> UnvalidatedTokens;
