9794770f0079dcc2aa49082db3886a2e361b313b
teoyuqi <teo.yuqi@gmail.com>
2023-09-10 19:15:54 +0800

    Remove unnecessary declarative clause

--- Team12/Code12/src/spa/src/qps/clause/declarativeClause/DeclarativeClause.cpp
+++
@@ -1,8 +0,0 @@
-#include "DeclarativeClause.h"
-
-#include <utility>
-
-DeclarativeClause::DeclarativeClause(Entity entity, Synonym synonym) :
-    entity(std::move(entity)),
-    synonym(std::move(synonym)) {}
-
