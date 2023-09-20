488210d4371f8535518d23fb9997d9560e1666e6
houtenteo <houtenteo@gmail.com>
2023-09-20 15:00:15 +0800

    add test cases for isIdent

--- Team12/Code12/src/unit_testing/src/qps/common/QPSStringUtilsTest.cpp
+++ Team12/Code12/src/unit_testing/src/qps/common/QPSStringUtilsTest.cpp
@@ -2,16 +2,20 @@
 
 #include "qps/common/QPSStringUtils.h"
 
+string validAlphabeticSynonym = "sdfksjdnkjfn";
+string validAlphanumericSynonym = "rnj324jn2k43";
+string validAlphabeticIdent = "\"fkjdgn\"";
+string validAlphanumericIdent = "\"j1nkn4j2n\"";
 
 TEST_CASE("test_isSynonym_allAlphabet_valid") {
-	string testSynonym = "sdfksjdnkjfn";
+	string testSynonym = validAlphabeticSynonym;
 	bool result = QPSStringUtils::isSynonym(testSynonym);
 
 	REQUIRE(result);
 }
 
 TEST_CASE("test_isSynonym_alphanumeric_startWithLetter_valid") {
-	string testSynonym = "rnj324jn2k43";
+	string testSynonym = validAlphanumericSynonym;
 	bool result = QPSStringUtils::isSynonym(testSynonym);
 
 	REQUIRE(result);
@@ -43,4 +47,53 @@ TEST_CASE("test_isSynonym_numeric_invalid") {
 	bool result = QPSStringUtils::isSynonym(testSynonym);
 
 	REQUIRE(result == false);
+}
+
+TEST_CASE("test_isIdent_allAlphabet_valid") {
+	string testIdent = validAlphabeticIdent;
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result);
+}
+
+TEST_CASE("test_isIdent_alphanumeric_startWithLetter_valid") {
+	string testIdent = validAlphanumericIdent;
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result);
+}
+
+TEST_CASE("test_isIdent_alphanumeric_startWithNumber_invalid") {
+	string testIdent = "3skdnf23j";
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result == false);
+}
+
+TEST_CASE("test_isIdent_containsSymbol_invalid") {
+	string testIdent = validAlphanumericIdent + "#";
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result == false);
+}
+
+TEST_CASE("test_isIdent_allNumbers_invalid") {
+	string testIdent = "\"34234\"";
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result == false);
+}
+
+TEST_CASE("test_isIdent_no_'\"'_invalid") {
+	string testIdent = validAlphanumericSynonym;
+	bool result = QPSStringUtils::isIdent(testIdent);
+
+	REQUIRE(result == false);
+}
+
+TEST_CASE("test_isEntRef_validSynonym_valid") {
+	string testEntRef = validAlphanumericSynonym;
+	bool result = QPSStringUtils::isEntRef(testEntRef);
+
+	REQUIRE(result);
 }
