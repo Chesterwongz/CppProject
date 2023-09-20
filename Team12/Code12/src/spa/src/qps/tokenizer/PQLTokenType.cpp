f39b05acdf53e975fa15328393e66e004b63c814
angkoonhwee <akoonhwee@gmail.com>
2023-09-20 21:50:05 +0800

    remove old folders and add tokenizer tests

---
+++ Team12/Code12/src/spa/src/qps/tokenizer/PQLTokenType.cpp
@@ -0,0 +1,15 @@
+#include "PQLTokenType.h"
+
+unordered_map<string, PQLTokenType> keywordToTokenType = {
+        { "stmt", PQL_ENTITY_TOKEN },
+        { "read", PQL_ENTITY_TOKEN },
+        { "print", PQL_ENTITY_TOKEN },
+        { "call", PQL_ENTITY_TOKEN },
+        { "while", PQL_ENTITY_TOKEN },
+        { "if", PQL_ENTITY_TOKEN },
+        { "assign", PQL_ENTITY_TOKEN },
+        { "variable", PQL_ENTITY_TOKEN },
+        { "constant", PQL_ENTITY_TOKEN },
+        { "procedure", PQL_ENTITY_TOKEN },
+        { "Select", PQL_SELECT_TOKEN }
+};
