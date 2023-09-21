a4e1157df22b9d59bcd1bce73301c9788585a18e
houtenteo <houtenteo@gmail.com>
2023-09-21 17:10:15 +0800

    add tests for argumentfactory

---
+++ Team12/Code12/src/unit_testing/src/qps/argument/ArgumentFactoryTest.cpp
@@ -0,0 +1,53 @@
+#include "catch.hpp"
+
+#include "qps/argument/argumentFactory/ArgumentFactory.h"
+
+string testWildcard = "_";
+string testSynonym = "test1234";
+string testIdent = "\"test4567\"";
+string testInteger = "4321";
+string testInvalid = "$234(NE";
+
+using std::unique_ptr;
+
+TEST_CASE("test_createArgument_wildcard") {
+	unique_ptr<IArgument> arg = ArgumentFactory::createArgument(testWildcard);
+
+	REQUIRE(arg->isIdent() == false);
+	REQUIRE(arg->isInteger() == false);
+	REQUIRE(arg->isSynonym() == false);
+	REQUIRE(arg->isWildcard());
+}
+
+TEST_CASE("test_createArgument_synonym") {
+	unique_ptr<IArgument> arg = ArgumentFactory::createArgument(testSynonym);
+
+	REQUIRE(arg->isIdent() == false);
+	REQUIRE(arg->isInteger() == false);
+	REQUIRE(arg->isSynonym());
+	REQUIRE(arg->isWildcard() == false);
+}
+
+TEST_CASE("test_createArgument_ident") {
+	unique_ptr<IArgument> arg = ArgumentFactory::createArgument(testIdent);
+
+	REQUIRE(arg->isIdent());
+	REQUIRE(arg->isInteger() == false);
+	REQUIRE(arg->isSynonym() == false);
+	REQUIRE(arg->isWildcard() == false);
+}
+
+TEST_CASE("test_createArgument_integer") {
+	unique_ptr<IArgument> arg = ArgumentFactory::createArgument(testInteger);
+
+	REQUIRE(arg->isIdent() == false);
+	REQUIRE(arg->isInteger());
+	REQUIRE(arg->isSynonym() == false);
+	REQUIRE(arg->isWildcard() == false);
+}
+
+TEST_CASE("test_createArgument_invalidString_returnsNullPtr") {
+	unique_ptr<IArgument> arg = ArgumentFactory::createArgument(testInvalid);
+
+	REQUIRE(arg == nullptr);
+}
