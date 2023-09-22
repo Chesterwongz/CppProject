c05074ca4b2274c2f358683441e1aed017166edb
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-09-22 21:05:39 +0800

    update import and remove ai prompt

--- Team12/Code12/src/spa/src/sp/extractors/FollowsExtractor.cpp
+++ Team12/Code12/src/spa/src/sp/extractors/FollowsExtractor.cpp
@@ -36,14 +36,6 @@ void FollowsExtractor::postVisitStmtList(const StmtListNode& node) {
 
 void FollowsExtractor::processCurrStmt(const StmtNode& node) {
     // ai-gen start(gpt-4, 2)
-    /*
-    prompt:
-    Given an AST and this definition for Follows/Follows*:
-    For any statements s1 and s2, where s1 and s2 are line numbers: Follows(s1, s2) holds if they are at the same nesting level, in the same statement list (stmtLst), and s2 appears in the program text immediately after s1.
-    Follows*(s1, s2) holds if Follows(s1, s2) or Follows(s1, s) and Follows*(s, s2) for some statement s.
-    Suppose I have a DesignExtractor class that contains the AST root node. How do you extract Follows* relationship from the AST using visitor pattern?
-    Follow-up: If the program has multiple statement blocks, e.g. while x { x = x + 1; y = y + 1; if x > 0 { x = 2; y = 2;}}, how does currStmtStack handle extract Follows* in that case
-     */
     int currLine = node.getLineNum();
     // Update the Follows* relationships for prev lines in this block
     for (const int& prevLine : nestingBlocksStack.top()) {
