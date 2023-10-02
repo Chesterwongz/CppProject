9654a068a4557ec2d250d8c85c6123914b841629
houtenteo <houtenteo@gmail.com>
2023-10-02 12:21:27 +0800

    Update AbstractionTestUtils.cpp

--- Team12/Code12/src/unit_testing/src/qps/abstraction/AbstractionTestUtils.cpp
+++ Team12/Code12/src/unit_testing/src/qps/abstraction/AbstractionTestUtils.cpp
@@ -2,12 +2,11 @@
 #include "qps/common/Keywords.h"
 
 unique_ptr<AbstractionParams> createMockAbstractionParams(
-        MockPKBReader &mockReader,
-        Context &mockContext,
-        Abstraction abstraction,
-        AbstractArgument &firstArgument,
-        AbstractArgument &secondArgument,
-        bool isTransitive) {
+    MockPKBReader& mockReader,
+    Context& mockContext,
+    Abstraction abstraction,
+    AbstractArgument& firstArgument,
+    AbstractArgument& secondArgument) {
     if (firstArgument.isSynonym()) {
         mockContext.addSynonym(firstArgument.getValue(), STMT_ENTITY);
     }
@@ -15,11 +14,11 @@ unique_ptr<AbstractionParams> createMockAbstractionParams(
         mockContext.addSynonym(secondArgument.getValue(), STMT_ENTITY);
     }
     unique_ptr<AbstractionParams> abstractionParams
-            = std::make_unique<AbstractionParams>(
-                    mockReader,
-                    mockContext,
-                    abstraction,
-                    firstArgument,
-                    secondArgument);
+        = std::make_unique<AbstractionParams>(
+            mockReader,
+            mockContext,
+            abstraction,
+            firstArgument,
+            secondArgument);
     return abstractionParams;
 }
