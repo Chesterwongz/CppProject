85c60a80421a8105161937248eb354eb6225f7c2
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-10-04 00:58:18 +0800

    format autotester

--- Team12/Code12/src/autotester/src/TestWrapper.h
+++ Team12/Code12/src/autotester/src/TestWrapper.h
@@ -1,35 +1,35 @@
 #ifndef TESTWRAPPER_H
 #define TESTWRAPPER_H
 
-#include <string>
+#include <algorithm>
 #include <iostream>
 #include <list>
 #include <memory>
+#include <string>
 #include <unordered_set>
-#include <algorithm>
 
 // include your other headers here
 #include "AbstractWrapper.h"
-#include "sp/SourceProcessor.h"
 #include "pkb/facade/PKB.h"
 #include "qps/QPS.h"
+#include "sp/SourceProcessor.h"
 
 class TestWrapper : public AbstractWrapper {
-private:
-    std::unique_ptr<QPS> qps;
-    std::unique_ptr<SourceProcessor> sourceProcessor;
-    std::unique_ptr<PKB> pkb;
+ private:
+  std::unique_ptr<QPS> qps;
+  std::unique_ptr<SourceProcessor> sourceProcessor;
+  std::unique_ptr<PKB> pkb;
 
  public:
   // default constructor
   TestWrapper();
-  
+
   // destructor
   ~TestWrapper();
-  
+
   // method for parsing the SIMPLE source
   virtual void parse(std::string filename);
-  
+
   // method for evaluating a query
   virtual void evaluate(std::string query, std::list<std::string>& results);
 };
