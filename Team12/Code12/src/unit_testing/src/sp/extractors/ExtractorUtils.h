aeb1e20124e65e4d707d9e7c34dc04f9443abcf1
Wu Xiaoyun <76084739+Xiaoyunnn@users.noreply.github.com>
2023-10-04 04:15:10 +0000

    update

--- Team12/Code12/src/unit_testing/src/sp/extractors/ExtractorUtils.h
+++ Team12/Code12/src/unit_testing/src/sp/extractors/ExtractorUtils.h
@@ -3,8 +3,10 @@
 #include <map>
 #include <memory>
 #include <set>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 #include <catch.hpp>
 
