a48350f9ad4b47841f61737a7f43cf766f438e70
angkoonhwee <akoonhwee@gmail.com>
2023-10-06 23:21:59 +0800

    run pre-commit hook

--- Team12/Code12/src/spa/src/qps/exceptions/QPSSemanticError.h
+++ Team12/Code12/src/spa/src/qps/exceptions/QPSSemanticError.h
@@ -2,13 +2,11 @@
 
 #include <string_view>
 
-const char QPS_SEMANTIC_ERR_REPEATED_SYNONYM[] =
-    "Repeated synonym during declaration: ";
+const char QPS_SEMANTIC_ERR_REPEATED_SYNONYM[] = "Repeated synonym during declaration: ";
 const char QPS_SEMANTIC_ERR_UNDECLARED_SYNONYM[] = "Using undeclared synonym: ";
 const char QPS_SEMANTIC_ERR_NOT_STMT_SYN[] = "Not a stmt synonym";
 const char QPS_SEMANTIC_ERR_NOT_VAR_SYN[] = "Not a var synonym";
-const char QPS_SEMANTIC_ERR_WILDCARD_FIRSTARG[] =
-    "Wildcard cannot be first argument";
+const char QPS_SEMANTIC_ERR_WILDCARD_FIRSTARG[] = "Wildcard cannot be first argument";
 
 class QPSSemanticError : public QPSException {
  public:
