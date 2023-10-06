f07e8353035ca3ee4ceeca3b1cf61dac93e610b9
houtenteo <houtenteo@gmail.com>
2023-10-06 21:22:43 +0800

    add integrated pattern tests for ms2
    add operator expression testing for partial matching
    add operator and no operator testing for exact matching

--- Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/SingleClauseAssignPatternTest.cpp
+++ Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/SingleClauseAssignPatternTest.cpp
@@ -10,10 +10,7 @@
 #include "qps/QPS.h"
 #include "sp/SourceProcessor.h"
 
-using std::string, std::unordered_map, std::map, std::unordered_set, std::set,
-    std::vector, std::pair;
-
-string ms1AssignSource =
+string ms2AssignSource =
     "procedure AssignPattern {"
     "  line1 = 1 + 2 + 3 + 4;"
     "  line2 = a + b -c * d /  e %f;"
@@ -23,120 +20,23 @@ string ms1AssignSource =
     "    line5 = a;"
     "    while ((((line6 )- 1) > (a - 1)) && (!(1 != 1))) {"
     "        line7 = a + 1;"
+    "        line8 = 2 + 3;"
     "    }"
     "  }"
     "}";
 
+// partial matching tests with operator since ms1 no operator for partial match
+
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "1") {
-  // wildcard first arg, partial match ident second arg
+  // wildcard first arg, partial match ident second arg (with operator)
   string query =
       "assign a;\n"
-      "Select a pattern a(_, _\"1\"_)";
+      "Select a pattern a(_, _\"1 + 2 + 3\"_)";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "4", "7"};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "2") {
-  // wildcard first arg, partial match ident second arg version 2
-  string query =
-      "assign a;\n"
-      "Select a pattern a(_, _\"a\"_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {"2", "5", "7"};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "3") {
-  // wildcard first arg, partial match ident second arg version 3 - no results
-  string query =
-      "assign a;\n"
-      "Select a pattern a(_, _\"huehue\"_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "4") {
-  // wildcard first arg, partial match ident second arg 
-  // select different assign
-  // return all assign statements since pattern evals to true
-  string query =
-      "assign a1, a2;\n"
-      "Select a1 pattern a2(_, _\"a\"_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "2", "4", "5", "7"};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "5") {
-  // wildcard first arg, partial match ident second arg 
-  // select different assign
-  // return none since pattern evals to false
-  string query =
-      "assign a1, a2;\n"
-      "Select a1 pattern a2(_, \"hehe\")";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "6") {
-  // wildcard first arg, wildcard second arg
-  string query =
-      "assign a;\n"
-      "Select a pattern a(_,_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "2", "4", "5", "7"};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "7") {
-  // ident first arg, partial match ident second arg
-  string query =
-      "assign a;\n"
-      "Select a pattern a(\"line1\", _\"1\"_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {"1"};
@@ -145,14 +45,113 @@ TEST_CASE(
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "8") {
-  // ident first arg, partial match ident second arg version 2
+    "2") {
+  // wildcard first arg, partial match ident second arg version 2 - no results
+  // (with operator)
   string query =
       "assign a;\n"
-      "Select a pattern a(\"line2\", _\"a\"_)";
+      "Select a pattern a(_, _\"2 + 3\"_)";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "3") {
+  // ident first arg, partial match ident second arg (with operator)
+  string query =
+      "assign a;\n"
+      "Select a pattern a(\"line2\", _\"c * d / e\"_)";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"2"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "4") {
+  // ident first arg, partial match ident second arg version 2 - no results
+  // (with operator)
+  string query =
+      "assign a;\n"
+      "Select a pattern a(\"line1\", _\"d / e\"_)";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "5") {
+  // synonym first arg, partial match ident second arg - select v (with operator)
+  string query =
+      "assign a; variable v;\n"
+      "Select v pattern a(v,_\"2 + 3\"_)";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"line8"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "6") {
+  // synonym first arg, partial match ident second arg - select a (with operator)
+  string query =
+      "assign a; variable v;\n"
+      "Select a pattern a(v,_\"2 + 3\"_)";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"8"};
+  REQUIRE(result == expected);
+}
+
+// Exact matching tests
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "7") {
+  // wildcard first arg, exact match ident second arg (no operator)
+  string query =
+      "assign a;\n"
+      "Select a pattern a(_, \"a\")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"5"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "8") {
+  // wildcard first arg, exact match ident second arg version 2 (with operator)
+  string query =
+      "assign a;\n"
+      "Select a pattern a(_, \"a+b-c*d/e%f\")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {"2"};
@@ -162,13 +161,14 @@ TEST_CASE(
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "9") {
-  // ident first arg, partial match ident second arg version 3 - no results
+  // wildcard first arg, exact match ident second arg version 3 - no results
+  // (no operator)
   string query =
       "assign a;\n"
-      "Select a pattern a(\"line1\", _\"12\"_)";
+      "Select a pattern a(_, \"2\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {};
@@ -178,13 +178,33 @@ TEST_CASE(
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "10") {
-  // ident first arg, partial match ident second arg version 4 - no results
+  // wildcard first arg, exact match ident second arg
+  // select different assign
+  // return all assign statements since pattern evals to true (no operator)
   string query =
-      "assign a;\n"
-      "Select a pattern a(\"line2\", _\"ab\"_)";
+      "assign a1, a2;\n"
+      "Select a1 pattern a2(_, \"1\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"1", "2", "4", "5", "7", "8"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "11") {
+  // wildcard first arg, exact match ident second arg 
+  // select different assign
+  // return none since pattern evals to false (with operator)
+  string query =
+      "assign a1, a2;\n"
+      "Select a1 pattern a2(_, \"1 + 2 + 3 + 4 + 5 + 6\")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {};
@@ -193,14 +213,30 @@ TEST_CASE(
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "11") {
-  // ident first arg, wildcard second arg
+    "12") {
+  // ident first arg, exact match ident second arg (with operator)
   string query =
       "assign a;\n"
-      "Select a pattern a(\"line4\",_)";
+      "Select a pattern a(\"line1\", \"1 + 2 + 3 + 4\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"1"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "13") {
+  // ident first arg, exact match ident second arg version 2 (no operator)
+  string query =
+      "assign a;\n"
+      "Select a pattern a(\"line4\", \"1\")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {"4"};
@@ -209,50 +245,15 @@ TEST_CASE(
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "12") {
-  // ident first arg, wildcard second arg version 2 - no results
-  string query =
-      "assign a; variable v;\n"
-      "Select a pattern a(\"v\",_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
-    "13") {
-  // ident first arg, partial match ident second arg
-  // select different assign
-  // return all assign statements since pattern evals to true
-  string query =
-      "assign a1, a2;\n"
-      "Select a1 pattern a2(\"line2\", _\"a\"_)";
-  SourceProcessor sp;
-  PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
-  QPS qps(pkb.getReader());
-  auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "2", "4", "5", "7"};
-  REQUIRE(result == expected);
-}
-
-TEST_CASE(
-    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "14") {
-  // ident first arg, partial match ident second arg
-  // select different assign
-  // return none since pattern evals to false
+  // ident first arg, exact match ident second arg version 3 - no results
+  // (with operator)
   string query =
-      "assign a1, a2;\n"
-      "Select a1 pattern a2(\"line2\", _\"ab\"_)";
+      "assign a;\n"
+      "Select a pattern a(\"line1\", \"12 + abc\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
   set<string> expected = {};
@@ -262,95 +263,116 @@ TEST_CASE(
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "15") {
-  // synonym first arg, wildcard second arg - select v
+  // ident first arg, exact match ident second arg version 4 - no results
+  // (no operator)
   string query =
-      "assign a; variable v;\n"
-      "Select v pattern a(v,_)";
+      "assign a;\n"
+      "Select a pattern a(\"line2\", \"ab\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"line1", "line2", "line4", "line5", "line7"};
+  set<string> expected = {};
   REQUIRE(result == expected);
 }
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "16") {
-  // synonym first arg, wildcard second arg - select a
+  // ident first arg, exact match ident second arg (no operator)
+  // select different assign
+  // return all assign statements since pattern evals to true
   string query =
-      "assign a; variable v;\n"
-      "Select a pattern a(v,_)";
+      "assign a1, a2;\n"
+      "Select a1 pattern a2(\"line5\", \"a\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "2", "4", "5", "7"};
+  set<string> expected = {"1", "2", "4", "5", "7", "8"};
   REQUIRE(result == expected);
 }
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "17") {
-  // synonym first arg, partial match ident second arg - select v
+  // ident first arg, exact match ident second arg (with operator)
+  // select different assign
+  // return none since pattern evals to false
   string query =
-      "assign a; variable v;\n"
-      "Select v pattern a(v,_\"1\"_)";
+      "assign a1, a2;\n"
+      "Select a1 pattern a2(\"line2\", \"a + a\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"line1", "line4", "line7"};
+  set<string> expected = {};
   REQUIRE(result == expected);
 }
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "18") {
-  // synonym first arg, partial match ident second arg - select a
+  // synonym first arg, exact match ident second arg - select v (no operator)
   string query =
       "assign a; variable v;\n"
-      "Select a pattern a(v,_\"1\"_)";
+      "Select v pattern a(v,_\"1\"_)";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"1", "4", "7"};
+  set<string> expected = {"line4"};
   REQUIRE(result == expected);
 }
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "19") {
-  // synonym first arg, partial match ident second arg version 2 - select v
+  // synonym first arg, exact match ident second arg - select a (no operator)
   string query =
       "assign a; variable v;\n"
-      "Select v pattern a(v,_\"a\"_)";
+      "Select a pattern a(v,_\"1\"_)";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"line2", "line5", "line7"};
+  set<string> expected = {"4"};
   REQUIRE(result == expected);
 }
 
 TEST_CASE(
     "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
     "20") {
-  // synonym first arg, partial match ident second arg version 2 - select a
+  // synonym first arg, exact match ident second arg - select v (with operator)
   string query =
       "assign a; variable v;\n"
-      "Select a pattern a(v,_\"a\"_)";
+      "Select v pattern a(v,\"a + 1\")";
   SourceProcessor sp;
   PKB pkb;
-  sp.processContent(ms1AssignSource, pkb.getWriter());
+  sp.processContent(ms2AssignSource, pkb.getWriter());
   QPS qps(pkb.getReader());
   auto result = qps.processQueryString(query);
-  set<string> expected = {"2", "5", "7"};
+  set<string> expected = {"line7"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS tests/Milestone1/SingleClauseTests/AssignPattern_queries.txt - "
+    "21") {
+  // synonym first arg, exact match ident second arg - select a (with operator)
+  string query =
+      "assign a; variable v;\n"
+      "Select a pattern a(v,\"a + 1\")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(ms2AssignSource, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  set<string> expected = {"7"};
   REQUIRE(result == expected);
 }
