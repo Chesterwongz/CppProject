b256b5d640aa18b23b34462da7900701b5c7a3b4
Chester Wong <psyforce99@gmail.com>
2023-10-15 00:35:49 +0800

    [rewrite-pkb] add truth predicate

--- Team12/Code12/src/spa/src/pkb/utils/PredicateUtils.h
+++ Team12/Code12/src/spa/src/pkb/utils/PredicateUtils.h
@@ -12,4 +12,9 @@ class PredicateUtils {
   static std::function<bool(T)> isInSet(const std::unordered_set<T>& set) {
     return [set](T val) { return set.count(val); };
   }
+
+  template <typename T>
+  static std::function<bool(T)> truePredicate() {
+    return [](const T& val) { return true; };
+  }
 };
