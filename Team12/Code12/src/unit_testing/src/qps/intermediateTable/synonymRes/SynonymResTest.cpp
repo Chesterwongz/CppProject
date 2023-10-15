db7e61570d552f24650662b81479a1ea26385539
teoyuqi <teo.yuqi@gmail.com>
2023-10-15 16:49:17 +0800

    add SynonymRes unit testing

--- Team12/Code12/src/unit_testing/src/qps/intermediateTable/synonymRes/SynonymResTest.cpp
+++ Team12/Code12/src/unit_testing/src/qps/intermediateTable/synonymRes/SynonymResTest.cpp
@@ -1,7 +1,116 @@
-#include <iostream>
 #include <catch.hpp>
 
+#include "../testData/SynonymResTestData.h"
 #include "qps/intermediateTable/synonymRes/SynonymRes.h"
 
+TEST_CASE("SynonymRes - operator== -between_same_kind") {
+  // between default synonyms - diff default value
+  REQUIRE(MOCK_DEFAULT_SYN_1 == SynonymRes::buildDefaultSynonym(SYNONYM_VAL_1));
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_DEFAULT_SYN_2);
 
-SynonymRes MOCK_SYNONYM_RES("
+  // between calls synonyms
+  REQUIRE(MOCK_CALLS_SYN_1 ==
+          SynonymRes::buildCallsSynonym(SYNONYM_VAL_1, SYNONYM_VAL_1B));
+  //  - diff call stmtNum but same procName
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_CALLS_SYN_2);
+  //  - diff call procName but same stmtNum
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_CALLS_SYN_3);
+  //  - diff call procName and diff stmtNum
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_CALLS_SYN_4);
+
+  // between constant synonyms - diff constant value
+  REQUIRE(MOCK_CONSTANT_SYN_1 ==
+          SynonymRes::buildConstantSynonym(SYNONYM_VAL_1));
+  REQUIRE_FALSE(MOCK_CONSTANT_SYN_1 == MOCK_CONSTANT_SYN_2);
+
+  // between proc synonyms - diff proc procName
+  REQUIRE(MOCK_PROC_SYN_1 == SynonymRes::buildProcSynonym(SYNONYM_VAL_1));
+  REQUIRE_FALSE(MOCK_PROC_SYN_1 == MOCK_PROC_SYN_2);
+
+  // between stmt synonyms - diff stmt stmtNum
+  REQUIRE(MOCK_STMT_SYN_1 == SynonymRes::buildStmtSynonym(SYNONYM_VAL_1));
+  REQUIRE_FALSE(MOCK_STMT_SYN_1 == MOCK_STMT_SYN_2);
+
+  // diff var stmtNum
+  REQUIRE(MOCK_VAR_SYN_1 == SynonymRes::buildVarSynonym(SYNONYM_VAL_1));
+  REQUIRE_FALSE(MOCK_VAR_SYN_1 == MOCK_VAR_SYN_2);
+}
+
+TEST_CASE("SynonymRes - operator== -between_diff_kind") {
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_CALLS_SYN_1);
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_CONSTANT_SYN_1);
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_PROC_SYN_1);
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_STMT_SYN_1);
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 == MOCK_VAR_SYN_1);
+
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_CONSTANT_SYN_1);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_PROC_SYN_1);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_STMT_SYN_1);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 == MOCK_VAR_SYN_1);
+
+  REQUIRE_FALSE(MOCK_CONSTANT_SYN_1 == MOCK_PROC_SYN_1);
+  REQUIRE_FALSE(MOCK_CONSTANT_SYN_1 == MOCK_STMT_SYN_1);
+  REQUIRE_FALSE(MOCK_CONSTANT_SYN_1 == MOCK_VAR_SYN_1);
+
+  REQUIRE_FALSE(MOCK_PROC_SYN_1 == MOCK_STMT_SYN_1);
+  REQUIRE_FALSE(MOCK_PROC_SYN_1 == MOCK_VAR_SYN_1);
+
+  REQUIRE_FALSE(MOCK_STMT_SYN_1 == MOCK_VAR_SYN_1);
+}
+
+TEST_CASE("SynonymRes - operator!= -between_same_kind") {
+  // between default synonyms - diff default value
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_DEFAULT_SYN_2);
+  REQUIRE_FALSE(MOCK_DEFAULT_SYN_1 != MOCK_DEFAULT_SYN_1);
+
+  // between calls synonyms - diff call stmtNum but same procName
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_2);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_1);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_2 != MOCK_CALLS_SYN_2);
+
+  // between calls synonyms - diff call procName but same stmtNum
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_3);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_1);
+
+  // between calls synonyms - diff call procName and diff stmtNum
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_4);
+  REQUIRE_FALSE(MOCK_CALLS_SYN_1 != MOCK_CALLS_SYN_1);
+
+  // between constant synonyms - diff constant value
+  REQUIRE(MOCK_CONSTANT_SYN_1 != MOCK_CONSTANT_SYN_2);
+  REQUIRE_FALSE(MOCK_CONSTANT_SYN_1 != MOCK_CONSTANT_SYN_1);
+
+  // between proc synonyms - diff proc procName
+  REQUIRE(MOCK_PROC_SYN_1 != MOCK_PROC_SYN_2);
+  REQUIRE_FALSE(MOCK_PROC_SYN_1 != MOCK_PROC_SYN_1);
+
+  // between stmt synonyms - diff stmt stmtNum
+  REQUIRE(MOCK_STMT_SYN_1 != MOCK_STMT_SYN_2);
+  REQUIRE_FALSE(MOCK_STMT_SYN_1 != MOCK_STMT_SYN_1);
+
+  // diff var stmtNum
+  REQUIRE(MOCK_VAR_SYN_1 != MOCK_VAR_SYN_2);
+  REQUIRE_FALSE(MOCK_VAR_SYN_1 != MOCK_VAR_SYN_1);
+}
+
+TEST_CASE("SynonymRes - operator!= -between_diff_kind") {
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_CALLS_SYN_1);
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_CONSTANT_SYN_1);
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_PROC_SYN_1);
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_STMT_SYN_1);
+  REQUIRE(MOCK_DEFAULT_SYN_1 != MOCK_VAR_SYN_1);
+
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_CONSTANT_SYN_1);
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_PROC_SYN_1);
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_STMT_SYN_1);
+  REQUIRE(MOCK_CALLS_SYN_1 != MOCK_VAR_SYN_1);
+
+  REQUIRE(MOCK_CONSTANT_SYN_1 != MOCK_PROC_SYN_1);
+  REQUIRE(MOCK_CONSTANT_SYN_1 != MOCK_STMT_SYN_1);
+  REQUIRE(MOCK_CONSTANT_SYN_1 != MOCK_VAR_SYN_1);
+
+  REQUIRE(MOCK_PROC_SYN_1 != MOCK_STMT_SYN_1);
+  REQUIRE(MOCK_PROC_SYN_1 != MOCK_VAR_SYN_1);
+
+  REQUIRE(MOCK_STMT_SYN_1 != MOCK_VAR_SYN_1);
+}
--- Team12/Code12/src/unit_testing/src/qps/intermediateTable/testData/SynonymResTestData.h
+++ Team12/Code12/src/unit_testing/src/qps/intermediateTable/testData/SynonymResTestData.h
@@ -5,15 +5,58 @@
 #include <utility>
 #include <vector>
 
-#include "qps/common/Keywords.h"
 #include "StringTestData.h"
+#include "qps/common/Keywords.h"
 #include "qps/intermediateTable/synonymRes/SynonymRes.h"
 
 using std::string, std::pair, std::vector, std::set;
 
-SynonymRes MOCK_SYNONYM_RES_A("A");
-SynonymRes MOCK_SYNONYM_RES_B("B");
-SynonymRes MOCK_SYNONYM_RES_C("C");
-SynonymRes MOCK_SYNONYM_RES_D("D");
+inline string SYNONYM_VAL_1 = "synonym value 1";
+inline string SYNONYM_VAL_2 = "synonym value 2";
+inline string SYNONYM_VAL_3 = "synonym value 3";
+inline string SYNONYM_VAL_4 = "synonym value 4";
 
+inline string SYNONYM_VAL_1B = "synonym value 1b";
+inline string SYNONYM_VAL_2B = "synonym value 2b";
 
+inline SynonymRes MOCK_DEFAULT_SYN_1 =
+    SynonymRes::buildDefaultSynonym(SYNONYM_VAL_1);
+inline SynonymRes MOCK_DEFAULT_SYN_2 =
+    SynonymRes::buildDefaultSynonym(SYNONYM_VAL_2);
+inline SynonymRes MOCK_DEFAULT_SYN_3 =
+    SynonymRes::buildDefaultSynonym(SYNONYM_VAL_3);
+inline SynonymRes MOCK_DEFAULT_SYN_4 =
+    SynonymRes::buildDefaultSynonym(SYNONYM_VAL_4);
+
+inline SynonymRes MOCK_CALLS_SYN_1 =
+    SynonymRes::buildCallsSynonym(SYNONYM_VAL_1, SYNONYM_VAL_1B);
+inline SynonymRes MOCK_CALLS_SYN_2 =
+    SynonymRes::buildCallsSynonym(SYNONYM_VAL_2, SYNONYM_VAL_1B);
+inline SynonymRes MOCK_CALLS_SYN_3 =
+    SynonymRes::buildCallsSynonym(SYNONYM_VAL_1, SYNONYM_VAL_2B);
+inline SynonymRes MOCK_CALLS_SYN_4 =
+    SynonymRes::buildCallsSynonym(SYNONYM_VAL_4, SYNONYM_VAL_2B);
+
+inline SynonymRes MOCK_CONSTANT_SYN_1 =
+    SynonymRes::buildConstantSynonym(SYNONYM_VAL_1);
+inline SynonymRes MOCK_CONSTANT_SYN_2 =
+    SynonymRes::buildConstantSynonym(SYNONYM_VAL_2);
+inline SynonymRes MOCK_CONSTANT_SYN_3 =
+    SynonymRes::buildConstantSynonym(SYNONYM_VAL_3);
+inline SynonymRes MOCK_CONSTANT_SYN_4 =
+    SynonymRes::buildConstantSynonym(SYNONYM_VAL_4);
+
+inline SynonymRes MOCK_PROC_SYN_1 = SynonymRes::buildProcSynonym(SYNONYM_VAL_1);
+inline SynonymRes MOCK_PROC_SYN_2 = SynonymRes::buildProcSynonym(SYNONYM_VAL_2);
+inline SynonymRes MOCK_PROC_SYN_3 = SynonymRes::buildProcSynonym(SYNONYM_VAL_3);
+inline SynonymRes MOCK_PROC_SYN_4 = SynonymRes::buildProcSynonym(SYNONYM_VAL_4);
+
+inline SynonymRes MOCK_STMT_SYN_1 = SynonymRes::buildStmtSynonym(SYNONYM_VAL_1);
+inline SynonymRes MOCK_STMT_SYN_2 = SynonymRes::buildStmtSynonym(SYNONYM_VAL_2);
+inline SynonymRes MOCK_STMT_SYN_3 = SynonymRes::buildStmtSynonym(SYNONYM_VAL_3);
+inline SynonymRes MOCK_STMT_SYN_4 = SynonymRes::buildStmtSynonym(SYNONYM_VAL_4);
+
+inline SynonymRes MOCK_VAR_SYN_1 = SynonymRes::buildVarSynonym(SYNONYM_VAL_1);
+inline SynonymRes MOCK_VAR_SYN_2 = SynonymRes::buildVarSynonym(SYNONYM_VAL_2);
+inline SynonymRes MOCK_VAR_SYN_3 = SynonymRes::buildVarSynonym(SYNONYM_VAL_3);
+inline SynonymRes MOCK_VAR_SYN_4 = SynonymRes::buildVarSynonym(SYNONYM_VAL_4);
