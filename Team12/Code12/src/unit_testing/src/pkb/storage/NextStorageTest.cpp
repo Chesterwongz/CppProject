dc2bbc90bc2f47588b093271ec568368940f3995
Chester Wong <psyforce99@gmail.com>
2023-10-18 21:20:49 +0800

    update NextStorageTest

--- Team12/Code12/src/unit_testing/src/pkb/storage/NextStorageTest.cpp
+++ Team12/Code12/src/unit_testing/src/pkb/storage/NextStorageTest.cpp
@@ -1,17 +1,17 @@
+#include <catch.hpp>
 #include <utility>
 #include <vector>
-#include <catch.hpp>
 
 #include "common/AliasTypes.h"
 #include "common/utils/PairUtils.h"
-#include "pkb/storage/relation_storage/stmt_to_stmt/NextStorage.h"
+#include "pkb/storage/NextStore.h"
 
 using std::vector, std::pair;
 
-void addNextRelationships(NextStorage& storage, IntToIntSetMap& map) {
+void addNextRelationships(NextStore& storage, IntToIntSetMap& map) {
   for (const auto& [from, toSet] : map) {
     for (int to : toSet) {
-      storage.addNext(from, to);
+      storage.addRelation(from, to);
     }
   }
 }
@@ -29,12 +29,16 @@ std::unordered_set<pair<int, int>, PairUtils::PairHash> getExpectedNextPairs(
 
 // ai-gen start (gpt-4, 1)
 template <typename Func>
-void testIntSetEquality(const std::unordered_map<int, IntSet>& data,
+bool testIntSetEquality(const std::unordered_map<int, IntSet>& data,
                         Func getStmts) {
   for (const auto& [key, expectedSet] : data) {
     IntSet actual = getStmts(key);
-    REQUIRE(actual == expectedSet);
+    if (actual != expectedSet) {
+      REQUIRE(actual == expectedSet);
+      return false;
+    }
   }
+  return true;
 }
 
 template <typename Func>
@@ -48,8 +52,8 @@ void testNestedIntSets(const std::unordered_map<int, IntSet>& data,
 }
 // ai-gen end
 
-TEST_CASE("NextStorage - 1 proc") {
-  NextStorage storage;
+TEST_CASE("NextStore - 1 proc") {
+  NextStore storage;
   IntToIntSetMap nextStmts = {{1, {2}}, {2, {3, 6}}, {3, {4, 5}}, {4, {3}},
                               {5, {8}}, {6, {7}},    {7, {8}}};
   IntToIntSetMap prevStmts = {{2, {1}}, {3, {2, 4}}, {4, {3}},   {5, {3}},
@@ -71,60 +75,65 @@ TEST_CASE("NextStorage - 1 proc") {
       {8, {1, 2, 3, 4, 5, 6, 7}},
   };
   addNextRelationships(storage, nextStmts);
+  storage.getRelationsT();
 
-  SECTION("getNextStmts") {
-    testIntSetEquality(nextStmts,
-                       [&](int from) { return storage.getNextStmts(from); });
-    REQUIRE(storage.getNextStmts(8).empty());
+  SECTION("getDirectSuccessors") {
+    REQUIRE(testIntSetEquality(nextStmts, [&](int from) {
+      return storage.getDirectSuccessors(from);
+    }));
+    REQUIRE(!storage.hasDirectSuccessor(8));
   }
 
-  SECTION("getNextTStmts") {
-    testIntSetEquality(expectedNextTStmts,
-                       [&](int from) { return storage.getNextTStmts(from); });
-    REQUIRE(storage.getNextTStmts(8).empty());
+  SECTION("getSuccessorsT") {
+    REQUIRE(testIntSetEquality(expectedNextTStmts, [&](int from) {
+      return storage.getSuccessorsT(from);
+    }));
+    REQUIRE(!storage.hasSuccessorsT(8));
   }
 
-  SECTION("getPrevStmts") {
-    testIntSetEquality(prevStmts,
-                       [&](int to) { return storage.getPrevStmts(to); });
-    REQUIRE(storage.getPrevStmts(1).empty());
+  SECTION("getDirectAncestors") {
+    REQUIRE(testIntSetEquality(
+        prevStmts, [&](int to) { return storage.getDirectAncestors(to); }));
+    REQUIRE(!storage.hasDirectAncestor(1));
   }
 
-  SECTION("getPrevTStmts") {
-    testIntSetEquality(expectedPrevTStmts,
-                       [&](int to) { return storage.getPrevTStmts(to); });
-    REQUIRE(storage.getPrevTStmts(1).empty());
+  SECTION("getAncestorsT") {
+    REQUIRE(testIntSetEquality(expectedPrevTStmts, [&](int to) {
+      if (storage.hasAncestorsT(to)) return storage.getAncestorsT(to);
+    }));
+    REQUIRE(!storage.hasAncestorsT(1));
   }
 
-  SECTION("isNext") {
-    testNestedIntSets(
-        nextStmts, [&](int from, int to) { return storage.isNext(from, to); });
-    REQUIRE(!storage.isNext(1, 3));
-    REQUIRE(!storage.isNext(6, 8));
-  }
-
-  SECTION("isNextT") {
-    testNestedIntSets(expectedNextTStmts, [&](int from, int to) {
-      return storage.isNextT(from, to);
+  SECTION("hasDirectRelation") {
+    testNestedIntSets(nextStmts, [&](int from, int to) {
+      return storage.hasDirectRelation(from, to);
     });
-    REQUIRE(!storage.isNextT(5, 6));
-    REQUIRE(!storage.isNextT(5, 7));
-    REQUIRE(!storage.isNextT(7, 1));
-    REQUIRE(!storage.isNextT(8, 8));
+    REQUIRE(!storage.hasDirectRelation(1, 3));
+    REQUIRE(!storage.hasDirectRelation(6, 8));
   }
 
-  SECTION("getNextPairs") {
+  SECTION("hasRelationT") {
+    testNestedIntSets(expectedNextTStmts, [&](int from, int to) {
+      return storage.hasRelationT(from, to);
+    });
+    REQUIRE(!storage.hasRelationT(5, 6));
+    REQUIRE(!storage.hasRelationT(5, 7));
+    REQUIRE(!storage.hasRelationT(7, 1));
+    REQUIRE(!storage.hasRelationT(8, 8));
+  }
+
+  SECTION("getAllDirectRelations") {
     std::unordered_set<pair<int, int>, PairUtils::PairHash> expected =
         getExpectedNextPairs(nextStmts);
-    vector<pair<int, int>> actual = storage.getNextPairs();
+    vector<pair<int, int>> actual = storage.getAllDirectRelations();
     std::unordered_set<pair<int, int>, PairUtils::PairHash> actualSet(
         actual.begin(), actual.end());
     REQUIRE(actualSet == expected);
   }
 }
 
-TEST_CASE("NextStorage - multiple procs") {
-  NextStorage storage;
+TEST_CASE("NextStore - multiple procs") {
+  NextStore storage;
   IntToIntSetMap nextStmts = {{1, {2}},   {2, {3}},   {4, {5, 7}}, {5, {6}},
                               {6, {9}},   {7, {8}},   {8, {9}},    {10, {11}},
                               {13, {14}}, {14, {15}}, {16, {17}},  {18, {19}}};
@@ -150,53 +159,54 @@ TEST_CASE("NextStorage - multiple procs") {
 
   addNextRelationships(storage, nextStmts);
 
-  SECTION("getNextStmts") {
-    testIntSetEquality(nextStmts,
-                       [&](int from) { return storage.getNextStmts(from); });
-    REQUIRE(storage.getNextStmts(3).empty());
-    REQUIRE(storage.getNextStmts(9).empty());
+  SECTION("getDirectSuccessors") {
+    testIntSetEquality(
+        nextStmts, [&](int from) { return storage.getDirectSuccessors(from); });
+    REQUIRE(storage.getDirectSuccessors(3).empty());
+    REQUIRE(storage.getDirectSuccessors(9).empty());
   }
 
-  SECTION("getNextTStmts") {
+  SECTION("getSuccessorsT") {
     testIntSetEquality(expectedNextTStmts,
-                       [&](int from) { return storage.getNextTStmts(from); });
-    REQUIRE(storage.getNextStmts(3).empty());
-    REQUIRE(storage.getNextStmts(9).empty());
+                       [&](int from) { return storage.getSuccessorsT(from); });
+    REQUIRE(storage.getDirectSuccessors(3).empty());
+    REQUIRE(storage.getDirectSuccessors(9).empty());
   }
 
-  SECTION("getPrevStmts") {
+  SECTION("getDirectAncestors") {
     testIntSetEquality(prevStmts,
-                       [&](int to) { return storage.getPrevStmts(to); });
-    REQUIRE(storage.getPrevStmts(1).empty());
+                       [&](int to) { return storage.getDirectAncestors(to); });
+    REQUIRE(storage.getDirectAncestors(1).empty());
   }
 
-  SECTION("getPrevTStmts") {
+  SECTION("getAncestorsT") {
     testIntSetEquality(expectedPrevTStmts,
-                       [&](int to) { return storage.getPrevTStmts(to); });
-    REQUIRE(storage.getPrevTStmts(1).empty());
+                       [&](int to) { return storage.getAncestorsT(to); });
+    REQUIRE(storage.getAncestorsT(1).empty());
   }
 
-  SECTION("isNext") {
-    testNestedIntSets(
-        nextStmts, [&](int from, int to) { return storage.isNext(from, to); });
-    REQUIRE(!storage.isNext(1, 3));
-    REQUIRE(!storage.isNext(6, 8));
-  }
-
-  SECTION("isNextT") {
-    testNestedIntSets(expectedNextTStmts, [&](int from, int to) {
-      return storage.isNextT(from, to);
+  SECTION("hasDirectRelation") {
+    testNestedIntSets(nextStmts, [&](int from, int to) {
+      return storage.hasDirectRelation(from, to);
     });
-    REQUIRE(!storage.isNextT(5, 7));
-    REQUIRE(!storage.isNextT(5, 8));
-    REQUIRE(!storage.isNextT(7, 1));
-    REQUIRE(!storage.isNextT(8, 8));
+    REQUIRE(!storage.hasDirectRelation(1, 3));
+    REQUIRE(!storage.hasDirectRelation(6, 8));
   }
 
-  SECTION("getNextPairs") {
+  SECTION("hasRelationT") {
+    testNestedIntSets(expectedNextTStmts, [&](int from, int to) {
+      return storage.hasRelationT(from, to);
+    });
+    REQUIRE(!storage.hasRelationT(5, 7));
+    REQUIRE(!storage.hasRelationT(5, 8));
+    REQUIRE(!storage.hasRelationT(7, 1));
+    REQUIRE(!storage.hasRelationT(8, 8));
+  }
+
+  SECTION("getAllDirectRelations") {
     std::unordered_set<pair<int, int>, PairUtils::PairHash> expected =
         getExpectedNextPairs(nextStmts);
-    vector<pair<int, int>> actual = storage.getNextPairs();
+    vector<pair<int, int>> actual = storage.getAllDirectRelations();
     std::unordered_set<pair<int, int>, PairUtils::PairHash> actualSet(
         actual.begin(), actual.end());
     REQUIRE(actualSet == expected);
