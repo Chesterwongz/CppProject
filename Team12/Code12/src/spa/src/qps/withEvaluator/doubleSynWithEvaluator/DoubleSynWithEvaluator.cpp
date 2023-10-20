3505efeb995de5a5160b80b5950d006a798c2ebc
houtenteo <houtenteo@gmail.com>
2023-10-21 02:10:36 +0800

    add DoubleSynWithEvaluator

--- Team12/Code12/src/spa/src/qps/withEvaluator/doubleSynWithEvaluator/DoubleSynWithEvaluator.cpp
+++ Team12/Code12/src/spa/src/qps/withEvaluator/doubleSynWithEvaluator/DoubleSynWithEvaluator.cpp
@@ -1,29 +1,30 @@
-//#include <vector>
-//
-//#include "qps/clause/utils/ClauseConstants.h"
-//#include "qps/intermediateTable/IntermediateTableFactory.h"
-//#include "qps/withEvaluator/doubleSynWithEvaluator/DoubleSynWithEvaluator.h"
-//
-//IntermediateTable DoubleSynWithEvaluator::evaluate(PKBReader& pkbReader) {
-//  Entity firstEntityType = firstSynonymArg->getEntityType();
-//  AttrRef firstAttrRef = firstSynonymArg->getAttrRef();
-//
-//  Entity secondEntityType = secondSynonymArg->getEntityType();
-//  AttrRef secondAttrRef = secondSynonymArg->getAttrRef();
-//
-//  Entity_AttrRef_Permutation firstArgPermutation = getPermutation(
-//      firstEntityType, firstAttrRef);
-//
-//  Entity_AttrRef_Permutation secondArgPermutation = getPermutation(
-//      secondEntityType, secondAttrRef);
-//
-//  IntermediateTable firstPKBResult =
-//      withEvaluatorFuncMap[firstArgPermutation](pkbReader);
-//
-//  IntermediateTable secondPKBResult =
-//      withEvaluatorFuncMap[firstArgPermutation](pkbReader);
-//  
-//  // TODO(houten) integrate with yq for joining on specific attrref
-//  // return firstPKBResult.join(secondPKBResult, firstAttrRef, secondAttrRef)
-//  return IntermediateTableFactory::buildEmptyIntermediateTable();
-//}
+#include <vector>
+
+#include "qps/clause/utils/ClauseConstants.h"
+#include "qps/intermediateTable/IntermediateTableFactory.h"
+#include "qps/withEvaluator/doubleSynWithEvaluator/DoubleSynWithEvaluator.h"
+
+IntermediateTable DoubleSynWithEvaluator::evaluate() {
+  string firstSynonymValue = firstSynonymArg->getValue();
+  Entity firstEntityType = firstSynonymArg->getEntityType();
+  AttrRef firstAttrRef = firstSynonymArg->getAttrRef();
+
+  string secondSynonymValue = secondSynonymArg->getValue();
+  Entity secondEntityType = secondSynonymArg->getEntityType();
+  AttrRef secondAttrRef = secondSynonymArg->getAttrRef();
+
+  vector<SynonymRes> firstPKBResult =
+      withEvaluatorFuncMap[firstEntityType]();
+  IntermediateTable firstResult = IntermediateTableFactory::buildSingleColTable(
+      firstSynonymValue, firstPKBResult);
+
+
+  vector<SynonymRes> secondPKBResult =
+      withEvaluatorFuncMap[secondEntityType]();
+  IntermediateTable secondResult = IntermediateTableFactory::buildSingleColTable(
+      secondSynonymValue, secondPKBResult);
+  
+  // TODO(houten) integrate with yq for joining on specific attrref
+  // return firstPKBResult.join(secondPKBResult, firstAttrRef, secondAttrRef)
+  return IntermediateTableFactory::buildEmptyIntermediateTable();
+}
