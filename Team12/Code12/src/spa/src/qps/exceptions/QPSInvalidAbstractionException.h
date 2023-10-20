06f431bcbcb69787f8df238f496a32c0859b2890
houtenteo <houtenteo@gmail.com>
2023-10-21 02:53:46 +0800

    remove unused line

--- Team12/Code12/src/spa/src/qps/exceptions/QPSInvalidAbstractionException.h
+++ Team12/Code12/src/spa/src/qps/exceptions/QPSInvalidAbstractionException.h
@@ -10,8 +10,6 @@ const char QPS_INVALID_ABSTRACTION_ERR_UNKNOWN_ARG_PERMUTATION[] =
     "unknown argument permutation";
 const char QPS_INVALID_ABSTRACTION_ERR_UNKNOWN_ABSTRACTION[] =
     "unknown abstraction";
-const char QPS_INVALID_WHILE_EVALUATOR_EVALUATION[] =
-    ": does not support this method call";
 
 class QPSInvalidAbstractionException : public QPSException {
  public:
