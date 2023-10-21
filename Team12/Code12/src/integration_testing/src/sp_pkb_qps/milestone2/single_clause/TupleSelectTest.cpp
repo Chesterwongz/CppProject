0a6c3ede9ade5f39ef32640e782f715bc39e3572
teoyuqi <teo.yuqi@gmail.com>
2023-10-22 02:46:48 +0800

    fix lint errors

--- Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/TupleSelectTest.cpp
+++ Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/TupleSelectTest.cpp
@@ -1,9 +1,9 @@
-#include <catch.hpp>
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <catch.hpp>
 
 #include "pkb/facade/PKB.h"
 #include "qps/QPS.h"
