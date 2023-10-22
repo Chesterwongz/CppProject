928e2447c7993127c93c0a3602d7ac750edfdf6d
angkoonhwee <akoonhwee@gmail.com>
2023-10-22 13:50:13 +0800

    remove magic strings for sp extractor

--- Team12/Code12/src/spa/src/common/Constants.h
+++ Team12/Code12/src/spa/src/common/Constants.h
@@ -3,6 +3,9 @@
 #include "common/StmtTypes.h"
 
 namespace common {
+constexpr int FIRST_CHILD = 0;
+constexpr int LEFT_CHILD = 0;
+constexpr int RIGHT_CHILD = 0;
 constexpr int INVALID_STMT_NUM = -1;
 constexpr int CFG_END_STMT_NUM = 0;
 constexpr const char* INVALID_PROC_NAME = "#InvalidProc";
