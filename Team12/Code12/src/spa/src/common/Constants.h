22aff1a5d09936f09f44da34ef3df9247219bbe7
angkoonhwee <akoonhwee@gmail.com>
2023-10-22 14:06:05 +0800

    typo

--- Team12/Code12/src/spa/src/common/Constants.h
+++ Team12/Code12/src/spa/src/common/Constants.h
@@ -5,7 +5,7 @@
 namespace common {
 constexpr int FIRST_CHILD = 0;
 constexpr int LEFT_CHILD = 0;
-constexpr int RIGHT_CHILD = 0;
+constexpr int RIGHT_CHILD = 1;
 constexpr int INVALID_STMT_NUM = -1;
 constexpr int CFG_END_STMT_NUM = 0;
 constexpr const char* INVALID_PROC_NAME = "#InvalidProc";
