12b7e41522ba2f5f07918792e0a64297c73529a8
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-11-13 15:38:49 +0800

    fix lint and add tests

--- Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/WithClauseTest.cpp
+++ Team12/Code12/src/integration_testing/src/sp_pkb_qps/milestone2/single_clause/WithClauseTest.cpp
@@ -1,9 +1,9 @@
-#include <catch.hpp>
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <catch.hpp>
 
 #include "pkb/facade/PKB.h"
 #include "qps/QPS.h"
@@ -1198,6 +1198,21 @@ TEST_CASE(
     "SP-PKB-QPS ../../Tests12/Milestone2/SingleClauseTests/With_queries.txt - "
     "77") {
   string query = R"(stmt s; Select s with "a" = "a")";
+  SourceProcessor sp;
+  PKB pkb;
+  sp.processContent(source, pkb.getWriter());
+  QPS qps(pkb.getReader());
+  auto result = qps.processQueryString(query);
+  unordered_set<string> expected = {
+      "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",  "10", "11", "12",
+      "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"};
+  REQUIRE(result == expected);
+}
+
+TEST_CASE(
+    "SP-PKB-QPS ../../Tests12/Milestone2/SingleClauseTests/With_queries.txt - "
+    "78") {
+  string query = R"(stmt a; Select a with "a" = "a")";
   SourceProcessor sp;
   PKB pkb;
   sp.processContent(source, pkb.getWriter());
